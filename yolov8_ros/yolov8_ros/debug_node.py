054521be885e0404b4df11821b25c3ea79920e02
Jose Miguel <josemiguel.guerrero@urjc.es>
2024-02-15 22:55:40 +0100

    Adding author

--- yolov8_ros/yolov8_ros/debug_node.py
+++ yolov8_ros/yolov8_ros/debug_node.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2023  Miguel Ángel González Santamarta
+# Copyright (C) 2024  José Miguel Guerrero Hernández
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
